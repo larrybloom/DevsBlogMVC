6c9fb0769b99a1e10613c0cbaba11d1d2ce19b12
larrybloom <aniediabasi.joseph@decagon.dev>
2024-02-26 16:06:58 +0100

    g

--- DevsTutorialCenterMVC/Controllers/EditorController.cs
+++ DevsTutorialCenterMVC/Controllers/EditorController.cs
@@ -14,9 +14,9 @@ public class EditorController : Controller
         _editorService = editorService;
         _blogPostService = blogPostService;
     }
-    
+
     [HttpGet("")]
-    public async Task<IActionResult> Index(bool successState=false)
+    public async Task<IActionResult> Index(bool successState = false)
     {
         var model = await _editorService.GetPendingArticles();
         if (successState)
@@ -25,23 +25,23 @@ public class EditorController : Controller
         }
         return View(model);
     }
-    
+
     [HttpPost("approve/{articleId}")]
     public async Task<IActionResult> ApproveArticle(string articleId)
     {
         var isSuccessful = await _editorService.ApproveArticle(articleId);
         if (!isSuccessful) return BadRequest();
-        return RedirectToAction("Index", new {successState = true});
+        return RedirectToAction("Index", new { successState = true });
     }
-    
+
     [HttpPost("reject/{articleId}")]
     public async Task<IActionResult> RejectArticle(string articleId)
     {
         var isSuccessful = await _editorService.RejectArticle(articleId);
         if (!isSuccessful) return BadRequest();
-        return RedirectToAction("Index", new {successState = true});
+        return RedirectToAction("Index", new { successState = true });
     }
-    
+
     [HttpGet("articles/{articleId}")]
     public async Task<IActionResult> OpenArticle(string articleId)
     {
